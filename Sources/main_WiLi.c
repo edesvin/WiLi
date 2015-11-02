60186d93c334fa4471e5d42326e8fd07b4dbc792
zsie7e <zsie7e@gmail.com>
2015-11-02 09:05:09 -0600

    *Función Anti pinch

--- Sources/main_WiLi.c
+++ Sources/main_WiLi.c
@@ -10,7 +10,7 @@
 #define PB_AnPi 	SIU.GPDI[67].B.PDI
 
 #define ValTMR_0 		0x0000F9FF
-#define ValTMR_1 		0x01869FFF
+#define ValTMR_1 		0x1312CFFF
 
 volatile T_UBYTE rub_FlagValUpAut = 0;
 volatile T_UBYTE rub_FlagValUpMan = 0;
@@ -89,6 +89,25 @@ void isr(void){
 			rub_FlagValDownMan = 0;
 		}
 		////////////////////////////////////////////////
+		if(PB_AnPi && (rub_FlagValUpAut || rub_FlagValUpMan) && !rub_FlagValDownAut && !rub_FlagValDownMan){
+			ruw_CountAnPi++;
+			FValAutUP = 0;
+			if(ruw_CountAnPi >= 10){
+				rub_FlagValAnPi = 1;
+				ruw_CountAnPi = 0;
+				
+				rub_FlagValDownAut = 1;
+				rub_FlagValDownMan = 0;
+				rub_FlagValUpAut = 0;
+				rub_FlagValUpMan = 0;
+				FValAutDown = 1;
+				
+			}
+		}
+		else{
+			ruw_CountAnPi = 0;
+		}
+		////////////////////////////////////////////////
 		if(F400){
 			count400++;
 			if(count400>=400){
@@ -124,6 +143,8 @@ void initPIT(uint32_t LDVALOR_0, uint32_t LDVALOR_1){
 	PIT.CH[1].TFLG.B.TIF = 1;
 }
 /******************************************************************************/
+
+
 void main (void) {
 	
 	T_UWORD luw_i;
@@ -150,6 +171,8 @@ void main (void) {
 			if(Fled){
 				Fled = 0;
 				SIU.GPDO[lub_CountIndice].B.PDO = 1;
+				SIU.GPDO[10].B.PDO = 0;
+				SIU.GPDO[11].B.PDO = 1;
 				lub_CountIndice++;
 				if(lub_CountIndice >= 10){
 					lub_CountIndice = 10;
@@ -164,20 +187,42 @@ void main (void) {
 		else if(rub_FlagValDownAut || rub_FlagValDownMan){
 			if(Fled){
 				Fled = 0;
-				if(lub_CountIndice < 1) {
+				if(lub_CountIndice <= 1) {
 					lub_CountIndice = 1;
 					rub_FlagValDownAut = 0;
 					FValAutDown = 0;
+					SIU.GPDO[11].B.PDO = 1;
 				}
 				SIU.GPDO[lub_CountIndice-1].B.PDO = 0;
+				SIU.GPDO[10].B.PDO = 1;
+				SIU.GPDO[11].B.PDO = 0;
 				lub_CountIndice--;
-
+				
+				if(lub_CountIndice == 0 && rub_FlagValAnPi){
+					SIU.GPDO[11].B.PDO = 1;
+					PIT.CH[1].TCTRL.B.TEN = 0;
+					PIT.CH[1].TCTRL.B.TEN = 1;
+					PIT.CH[1].LDVAL.R = ValTMR_1;
+					PIT.CH[1].TFLG.B.TIF = 1;
+					rub_FlagValAnPi = 0;
+					while(!PIT.CH[1].TFLG.B.TIF);
+					PIT.CH[1].TFLG.B.TIF = 1;
+					Fled = 0;
+					rub_FlagValDownAut = 0;
+					rub_FlagValDownMan = 0;
+					rub_FlagValUpAut = 0;
+					rub_FlagValUpMan = 0;
+					FValAutDown = 0;
+					FValAutUP = 0;
+				}
 				
 			}
 		}
 		////////////////////////////////////////////////
 		else if(!rub_FlagValUpAut && !rub_FlagValUpMan && !rub_FlagValDownAut && !rub_FlagValDownMan){
 			Fled = 0;
+			SIU.GPDO[10].B.PDO = 1;
+			SIU.GPDO[11].B.PDO = 1;
 		}
 	}
 }
